966149effcb15bfc7ac34edae82aa056ce60304a
JoshCuben <70572983+JoshCuben@users.noreply.github.com>
2026-05-29 01:47:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,35 @@ document.querySelectorAll(".card").forEach(card => {
 });
 
 /* DARK MODE */
-document.getElementById("modeToggle").addEventListener("click", () => {
+
+const modeToggle = document.getElementById("modeToggle");
+
+modeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+});
+
+/* MOBILE MENU */
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+/* FLIP CARD */
+
+document.querySelectorAll(".card").forEach(card => {
+
+  const details = card.querySelector(".details");
+  const back = card.querySelector(".backBtn");
+
+  details.addEventListener("click", () => {
+    card.classList.add("active");
+  });
+
+  back.addEventListener("click", () => {
+    card.classList.remove("active");
+  });
+
 });
